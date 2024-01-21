9f92fde79f7ce82563c6ebd0f04420ababdf14cc
farismamnoon <farismamnoon1@gmail.com>
2024-01-21 17:08:12 +0530

    handleSubmitting

--- src/pages/class/form.js
+++ src/pages/class/form.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 // material-ui
@@ -20,270 +20,233 @@ import {
 
 // third party
 import * as Yup from "yup";
-import { Formik } from "formik";
+import { useFormik } from "formik";
+import axios from "axios";
 
 // project import
 import AnimateButton from "components/@extended/AnimateButton";
-import { visitLexicalEnvironment } from '../../../node_modules/typescript/lib/typescript';
+import { visitLexicalEnvironment } from "../../../node_modules/typescript/lib/typescript";
+import { base_url } from "utils/baseurl";
 
 // ============================|| FIREBASE - REGISTER ||============================ //
 
 const Class = () => {
-  const [ subjectName, setSubjectName] = useState([]);
+  const [subjectName, setSubjectName] = useState([]);
 
   const names = ["hai,", "bai", "go", "nbabi", "adfkj"];
 
-  const handleSelectChange = (formik) => (event) => {
+  const handleSelectChange = (event) => {
     const {
-
       target: { value },
     } = event;
 
-    formik.setFieldValue('subjects', value)
+    formikprops.setFieldValue("subjects", value);
 
     setSubjectName(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
+      typeof value === "string" ? value.split(",") : value
     );
   };
+
+  const formikprops = useFormik({
+    initialValues: {
+      className: "",
+      division: "",
+      teacher: "",
+      subjects: [],
+      batch: "",
+      fees: "",
+      submit: null,
+    },
+    validationSchema: Yup.object().shape({
+      className: Yup.number()
+        .typeError("Class should be a number")
+        .min(1, 'Minimum value is 1')
+        .required("Class is required"),
+      division: Yup.string().max(255),
+      teacher: Yup.string().max(255).required("A Teacher is required"),
+      subjects: Yup.string().max(255).required("Select the subjects"),
+      batch: Yup.number().required("Enter the batch year"),
+      fees: Yup.number().required("Enter the total fees of the class"),
+    }),
+    onSubmit: async (values, { setErrors, setStatus, setSubmitting }) => {
+      try {
+        console.log("values");
+        const response = await axios.post(`${base_url}/class/create`, values);
+
+        setStatus({ success: false });
+        setSubmitting(false);
+      } catch (err) {
+        console.error(err);
+        setStatus({ success: false });
+        setErrors({ submit: err.message });
+        setSubmitting(false);
+      }
+    },
+  });
   return (
     <>
-      <Formik
-        initialValues={{
-          firstname: "",
-          lastname: "",
-          subjects: [],
-          email: "",
-          company: "",
-          password: "",
-          submit: null,
-        }}
-        // validationSchema={Yup.object().shape({
-        //   firstname: Yup.string().max(255).required("First Name is required"),
-        //   lastname: Yup.string().max(255).required("Last Name is required"),
-        //   email: Yup.string()
-        //     .email("Must be a valid email")
-        //     .max(255)
-        //     .required("Email is required"),
-        //   password: Yup.string().max(255).required("Password is required"),
-        // })}
-        onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-          try {
-            console.log(values);
-            setStatus({ success: false });
-            setSubmitting(false);
-          } catch (err) {
-            console.error(err);
-            setStatus({ success: false });
-            setErrors({ submit: err.message });
-            setSubmitting(false);
-          }
-        }}
-      >
-        {({
-          errors,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-          isSubmitting,
-          touched,
-          values,
-        }) => (
-          <form noValidate onSubmit={handleSubmit}>
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="class-name">Class Standard</InputLabel>
-                  <OutlinedInput
-                    id="class-name"
-                    type="number"
-                    value={values.className}
-                    name="className"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    fullWidth
-                    error={Boolean(touched.className && errors.className)}
-                  />
-                  {touched.className && errors.className && (
-                    <FormHelperText error id="helper-text-className-signup">
-                      {errors.className}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="division">Division</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.division && errors.division)}
-                    id="division"
-                    value={values.division}
-                    name="division"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    inputProps={{}}
-                  />
-                  {touched.division && errors.division && (
-                    <FormHelperText error id="helper-text-division-signup">
-                      {errors.division}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="teacher">Class Teacher</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.teacher && errors.teacher)}
-                    id="teacher"
-                    value={values.teacher}
-                    name="teacher"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    inputProps={{}}
-                  />
-                  {touched.teacher && errors.teacher && (
-                    <FormHelperText error id="helper-text-teacher-signup">
-                      {errors.teacher}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="subjects">Subjects</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.subjects && errors.subjects)}
-                    id="subjects"
-                    value={values.subjects}
-                    name="subjects"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    inputProps={{}}
-                  />
-                  {touched.subjects && errors.subjects && (
-                    <FormHelperText error id="helper-text-subjects-signup">
-                      {errors.subjects}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
-                  <Select
-                    labelId="demo-multiple-chip-label"
-                    id="demo-multiple-chip"
-                    multiple
-                    value={subjectName}
-                    onChange={handleSelectChange(formik)}
-                    input={
-                      <OutlinedInput id="select-multiple-chip" label="Chip" />
-                    }
-                    renderValue={(selected) => (
-                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                        {selected.map((value) => (
-                          <Chip key={value} label={value} />
-                        ))}
-                      </Box>
-                    )}
-                    // MenuProps={MenuProps}
-                  >
-                    {names.map((name) => (
-                      <MenuItem
-                        key={name}
-                        value={name}
-                        // style={getStyles(name, personName, theme)}
-                      >
-                        {name}
-                      </MenuItem>
+      <form noValidate onSubmit={formikprops.handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="className">Class Standard</InputLabel>
+              <OutlinedInput
+                id="className"
+                type="number"
+                value={formikprops.values.className}
+                name="className"
+                onBlur={formikprops.handleBlur}
+                onChange={formikprops.handleChange}
+                fullWidth
+                error={Boolean(
+                  formikprops.touched.className && formikprops.errors.className
+                )}
+              />
+              {formikprops.touched.className &&
+                formikprops.errors.className && (
+                  <FormHelperText error id="helper-text-className-signup">
+                    {formikprops.errors.className}
+                  </FormHelperText>
+                )}
+            </Stack>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="division">Division</InputLabel>
+              <OutlinedInput
+                fullWidth
+                error={Boolean(
+                  formikprops.touched.division && formikprops.errors.division
+                )}
+                id="division"
+                value={formikprops.values.division}
+                name="division"
+                onBlur={formikprops.handleBlur}
+                onChange={formikprops.handleChange}
+                inputProps={{}}
+              />
+              {formikprops.touched.division && formikprops.errors.division && (
+                <FormHelperText error id="helper-text-division-signup">
+                  {formikprops.errors.division}
+                </FormHelperText>
+              )}
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="teacher">Class Teacher</InputLabel>
+              <OutlinedInput
+                fullWidth
+                error={Boolean(
+                  formikprops.touched.teacher && formikprops.errors.teacher
+                )}
+                id="teacher"
+                value={formikprops.values.teacher}
+                name="teacher"
+                onBlur={formikprops.handleBlur}
+                onChange={formikprops.handleChange}
+                inputProps={{}}
+              />
+              {formikprops.touched.teacher && formikprops.errors.teacher && (
+                <FormHelperText error id="helper-text-teacher-signup">
+                  {formikprops.errors.teacher}
+                </FormHelperText>
+              )}
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={1}>
+              <InputLabel id="subjects">Subjects</InputLabel>
+              <Select
+                labelId="subjects"
+                id="subjects"
+                multiple
+                value={formikprops.values.subjects}
+                onChange={handleSelectChange}
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                renderValue={(selected) => (
+                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
                     ))}
-                  </Select>
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="batch-start">Batch Start</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.batchStart && errors.batchStart)}
-                    id="batch-start"
-                    value={values.batchStart}
-                    name="batchStart"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="2019"
-                    inputProps={{}}
-                  />
-                  {touched.batchStart && errors.batchStart && (
-                    <FormHelperText error id="helper-text-batchStart-signup">
-                      {errors.batchStart}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="batch-end">Batch End</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.batchEnd && errors.batchEnd)}
-                    id="batch-end"
-                    value={values.batchEnd}
-                    name="batchEnd"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="2020"
-                    inputProps={{}}
-                  />
-                  {touched.batchEnd && errors.batchEnd && (
-                    <FormHelperText error id="helper-text-batchEnd-signup">
-                      {errors.batchEnd}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={6}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="fees">Fees</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.fees && errors.fees)}
-                    id="fees"
-                    value={values.fees}
-                    name="fees"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    inputProps={{}}
-                  />
-                  {touched.fees && errors.fees && (
-                    <FormHelperText error id="helper-text-fees-signup">
-                      {errors.fees}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={12}>
-                <AnimateButton>
-                  <Button
-                    disableElevation
-                    disabled={isSubmitting}
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                    color="primary"
+                  </Box>
+                )}
+                // MenuProps={MenuProps}
+              >
+                {names.map((name) => (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    // style={getStyles(name, personName, theme)}
                   >
-                    Submit
-                  </Button>
-                </AnimateButton>
-              </Grid>
-            </Grid>
-          </form>
-        )}
-      </Formik>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="batch">Batch</InputLabel>
+              <OutlinedInput
+                fullWidth
+                error={Boolean(
+                  formikprops.touched.batch && formikprops.errors.batch
+                )}
+                id="batch"
+                value={formikprops.values.batch}
+                name="batch"
+                onBlur={formikprops.handleBlur}
+                onChange={formikprops.handleChange}
+                placeholder="2019"
+                inputProps={{}}
+              />
+              {formikprops.touched.batch && formikprops.errors.batch && (
+                <FormHelperText error id="helper-text-batch-signup">
+                  {formikprops.errors.batch}
+                </FormHelperText>
+              )}
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="fees">Fees</InputLabel>
+              <OutlinedInput
+                fullWidth
+                error={Boolean(
+                  formikprops.touched.fees && formikprops.errors.fees
+                )}
+                id="fees"
+                value={formikprops.values.fees}
+                name="fees"
+                onBlur={formikprops.handleBlur}
+                onChange={formikprops.handleChange}
+                inputProps={{}}
+              />
+              {formikprops.touched.fees && formikprops.errors.fees && (
+                <FormHelperText error id="helper-text-fees-signup">
+                  {formikprops.errors.fees}
+                </FormHelperText>
+              )}
+            </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <AnimateButton>
+              <Button
+                disableElevation
+                disabled={formikprops.isSubmitting}
+                size="large"
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Submit
+              </Button>
+            </AnimateButton>
+          </Grid>
+        </Grid>
+      </form>
     </>
   );
 };
